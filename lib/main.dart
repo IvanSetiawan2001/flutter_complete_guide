e2c27ac22d669747e74c313d26be82b98864b176
Ivan Setiawan <fanset.raharjo@gmail.com>
2022-11-04 10:57:08 +0900

    Passing callback function

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -40,19 +42,9 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(question[_questionIndex]),
-            ElevatedButton(
-              onPressed: _answerQuestion,
-              child: Text('Answer 1'),
-            ),
-            ElevatedButton(
-              onPressed: () => print(
-                  'answer 2 choosen'), //another way to use function (anonymous function)
-              child: Text('Answer 2'),
-            ),
-            ElevatedButton(
-              onPressed: _answerQuestion,
-              child: Text('Answer 3'),
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
