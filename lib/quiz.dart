d4efed71314ab1333b353ef746cfbbafb553cf29
Ivan Setiawan <fanset.raharjo@gmail.com>
2022-11-07 10:37:55 +0900

    calculating the total score

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -18,9 +18,12 @@ class Quiz extends StatelessWidget {
     return Column(
       children: [
         Question(questions[questionIndex]['questionText']),
-        ...(questions[questionIndex]['answer'] as List<String>).map((answer) {
+        ...(questions[questionIndex]['answer'] as List<Map<String, Object>>)
+            .map((answer) {
           //.map to tell the code to execute the function with return value for every element in the map.
-          return Answer(answerQuestion, answer);
+          return Answer(() => answerQuestion(answer['score']), answer['text']);
+          //()=>answerQuestion mean we get the address to the answer question and save it in answer we use answerQuestion() because we want to call the function
+          //with the ()=> method, the function no longer will be called when render so we can call answerQuestion() instead of answerQuestion
         }).toList() // this is a list and we cannot add a list to a list, so we need to extract every value in the list and then add it to children list with "..."
       ],
     );
